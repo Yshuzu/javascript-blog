252569d252e9978ad60a9ccc60e13d6640e801c5
Sebastian Ziędalski <sebastian.ziedalski@gmail.com>
2025-02-04 19:39:14 +0100

    function titleClickHandler [done]

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@
     }); 
 */
 const titleClickHandler = function(event){
+    event.preventDefault();
     const clickedElement = this;
     console.log(event)
     console.log('Link was clicked!');
@@ -26,15 +27,18 @@ const activeArticles = document.querySelectorAll('.posts article.active');
 for(let activeArticle of activeArticles){
     activeArticle.classList.remove('active');
 }    
-    /* get 'href' attribute from the clicked link */
-  
-    /* find the correct article using the selector (value of 'href' attribute) */
-  
-    /* add class 'active' to the correct article */
+/* [DONE] get 'href' attribute from the clicked link */
+const articleSelector = clickedElement.getAttribute('href');
+console.log(articleSelector)
+/* [DONE] find the correct article using the selector (value of 'href' attribute) */
+const targetArticle = document.querySelector(articleSelector);
+console.log(targetArticle)  
+/* [DONE] add class 'active' to the correct article */
+targetArticle.classList.add('active');    
   }
   
-  const links = document.querySelectorAll('.titles a');
+const links = document.querySelectorAll('.titles a');
   
-  for(let link of links){
+for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
